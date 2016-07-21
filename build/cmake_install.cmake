e864b069dba5bca5832bea194578492e64f065c2
wuzhuobin <317854987@qq.com>
2016-07-21 20:36:16 +0800

    h

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/PlaqueQuant")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
