867f64e5c5c9449c3c4e4f22340894b9cf45619d
Matthew Wong <fromosia@gmail.com>
2017-09-01 19:34:59 -0700

    Add Comments

--- codes/Yolk3DSeries/Test/main.cpp
+++ codes/Yolk3DSeries/Test/main.cpp
@@ -23,12 +23,12 @@ int main(int argc, char** argv)
 
 	QApplication app(argc, argv);
 
-	MainWindow* mainwnd = new MainWindow;
-
+	/* Use GDCMSeriesFileNames to read all files a the argument directory*/
 	typedef itk::GDCMSeriesFileNames	InputNamesGeneratorType;
 	InputNamesGeneratorType::Pointer inputnamesGenerator = InputNamesGeneratorType::New();
 	inputnamesGenerator->SetInputDirectory(argv[1]);
 
+	/* Now excepts QStringList only */
 	std::vector<std::string> filenames(inputnamesGenerator->GetInputFileNames());
 	QStringList qfilenames;
 	for (std::vector<std::string>::const_iterator iter = filenames.cbegin();
@@ -36,31 +36,26 @@ int main(int argc, char** argv)
 	{
 		qfilenames.append(QString::fromStdString(*iter));
 	}
-	qfilenames.sort();
 
-
-	//QWidget* tempwidget = new QWidget;
-	//QGridLayout* layout = new QGridLayout(tempwidget);
-	//layout->addWidget(spinbox);
 	Yolk3DSeries* y = new Yolk3DSeries();
+
+	/* Create a spinbox to control which 3D projection slice */
 	QSpinBox* spinbox = new QSpinBox(y);
 	y->layout()->addWidget(spinbox);
-
 	spinbox->setMinimum(0);
 	spinbox->setMaximum(qfilenames.length() - 1);
-
-	double normal[3] = { 1, 1, 1 };
-	double origin[3] = { 0, 0, 10 };
+	QObject::connect(spinbox, SIGNAL(valueChanged(int)), y, SLOT(slotSetSlice(int)));
 
 	y->show();
 	y->set3DSeries(qfilenames);
 
+	/* Connect MyViewer object signal to the yolk widget */
+	MainWindow* mainwnd = new MainWindow;
 	y->connectViewer(mainwnd->GetCore()->Get2DViewer(0));
 	y->connectViewer(mainwnd->GetCore()->Get2DViewer(1));
 	y->connectViewer(mainwnd->GetCore()->Get2DViewer(2));
 
 	mainwnd->show();
 
-	QObject::connect(spinbox, SIGNAL(valueChanged(int)), y, SLOT(slotSetSlice(int)));
 	return app.exec();
 }
